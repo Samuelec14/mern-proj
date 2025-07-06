d4a6a5119ae1fead814a9fe9428a030b310ae60b
Samuelec14 <samuconf14@gmail.com>
2025-07-06 17:42:31 +0200

    creazione db, configurazione .env e connessione db

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,20 @@
 import express from "express"
 import notesRoute from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js"
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+const port = process.env.PORT || 5001
+
+connectDB();
 
 // middleware di percorso per le rotte dell'entità note 
 app.use("/api/notes", notesRoute)
 
-app.listen(5001, () => {
-    console.log("Server started on: http://localhost:5001")
+app.listen(port, () => {
+    console.log(`Server started on: http://localhost:${port}`)
 })
+
+

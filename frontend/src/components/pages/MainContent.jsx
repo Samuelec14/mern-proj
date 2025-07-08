da5275bd7510e741153d44eff78b0ddcd4cd85ac
Samuelec14 <samuconf14@gmail.com>
2025-07-08 11:03:35 +0200

    axios chiamate di esempio

--- frontend/src/components/pages/MainContent.jsx
+++ frontend/src/components/pages/MainContent.jsx
@@ -2,6 +2,52 @@ import React from 'react'
 
 
 const MainContent = () => {
+
+    const token = 'token';
+    const user_id = 3
+    const queryParams = {
+        limit: 10,
+        page: 2,
+        order_by: 'created_at_desc'
+    }
+    const bodyExample = {
+        user_id: 3,
+        name: 'riccardo',
+        last_name: 'rossi'
+    }
+
+
+    // get di esempio 
+    axios.get('url/di/esempio', {
+        params: queryParams,
+        headers: {
+            Authorization: token
+        }
+    })
+        .then(response => {
+            // istruzioni successive alla risposta
+        })
+        .catch(error => {
+            // istruzioni successive ad un errore 
+        })
+
+
+    // post di esempio
+    axios.post(`url/parametrico/${user_id}`, {
+        headers: {
+            Authorization: token,
+            "Content-Type" : "application/json"
+        },
+        body: bodyExample
+    })
+        .then(response => {
+            // istruzioni success true
+        })
+        .catch(error => {
+            // istruzioni success false 
+        })
+
+
     return (
         <div>MainContent</div>
     )
